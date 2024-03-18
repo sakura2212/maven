f2a057f8f4311a40af72992ed56c0512f051cb77
sakura2212 <89779458+sakura2212@users.noreply.github.com>
2024-03-18 17:57:20 +0530

    added

--- src/test/java/basic/OrganisationTest.java
+++ src/test/java/basic/OrganisationTest.java
@@ -1,18 +1,38 @@
 package basic;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import commonutils.PropertyFileUtil;
+import commonutils.WebDriverUtil;
 
 //class name => suffix mandatory Test
 public class OrganisationTest {
 
 	//method name => suffix mandatory Test
 	@Test
-	public void createOrgTest(){
+	public void createOrgTest() throws IOException{
+		PropertyFileUtil pf =new PropertyFileUtil() ;
+		WebDriverUtil wu = new WebDriverUtil() ;
+		
 		WebDriver driver =new ChromeDriver() ;
 		
-		driver.get("http://localhost:8888/");
+		//driver.get("http://localhost:8888/");
+		String url = pf.getDataFromPropertyFile("Url") ;
+		String userName = pf.getDataFromPropertyFile("Username") ;
+		String userPass = pf.getDataFromPropertyFile("Password") ;
+		
+		//launch appln
+				driver.get(url);
+				
+				//login cred
+				driver.findElement(By.name("user_name")).sendKeys(userName);
+				driver.findElement(By.name("user_password")).sendKeys(userPass);
+				driver.findElement(By.id("submitButton")).click();
 		
 	}
 }
